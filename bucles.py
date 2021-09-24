e58261b31cdcd74c93a32f5cbd64fbd0eec657fe
NataVilla <nataye7@gmail.com>
2021-09-24 17:17:36 -0500

    Natalia:primeros ejercicios python

---
+++ bucles.py
@@ -0,0 +1,55 @@
+# contador = 0
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 1
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 2
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 3
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 4
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 5
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# i = 2
+
+# while i < 100:
+#     i = 2**i
+#     if i == 100:
+#         continue
+#     print(i)
+
+
+
+# nombre_1 = input('¿Cual es tu nombre?: ')
+# edad_1 = input('¿Cual es tu edad?: ')
+# nombre_2 = input('¿Cual es el nombre de tu amigo?: ' )
+# edad_2 = input('¿Cual es la edad de tu amigo: ')
+
+# if  edad_1 < edad_2:
+#     print(nombre_1 + ' eres menor que tu amigo ' + nombre_2)
+# elif edad_1 > edad_2:
+#     print(nombre_1 + ' eres mayor que tu amigo '  +   nombre_2)
+# else:
+#     print(nombre_1  + ' tienes la misma edad que '  +  nombre_2)         
+
+
+
+# def fibonacci(n):
+#     print(n)
+#     if n == 0 or n == 1:
+#         return 1
+#     return fibonacci(n - 1) + fibonacci(n - 2) 
+# n = int(input('Escribe un numero: '))    
+# print( f'El numero entero fibonacci para {n} es {fibonacci(n)}')
+
+# for i in range(1, 100,2):
+#     print(i)
+
+
+
---
+++ conversor.py
@@ -0,0 +1,29 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input('¿Cuántos pesos '  +  tipo_pesos  +  ' tienes? ')
+    pesos = float(pesos)
+    dolares = pesos/valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $ ' + dolares + ' dolares')
+menu = """
+Bienvenido al conversor de monedas ✨✨
+
+1 - Pesos Colombianos
+2 - Pesos Argentino
+3 - Pesos Mexicanos
+
+Elige una opcion:  """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor('Colombianos',3900)
+
+elif opcion == 2:
+    conversor('Argentinos', 65)
+    
+elif opcion == 3:
+    conversor('Mexicanos', 24)
+
+else:
+    print('Ingresa una opción correcta')
